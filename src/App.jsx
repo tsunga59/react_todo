d9096b219ce24c6727d03d1764f6685e2b1b2353
tsunga59 <tatsumochi15@gmail.com>
2022-05-17 22:21:21 +0900

    ページの雛形を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
 import "./App.css";
 
-export const App = () => {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Todo App</h1>
-    </div>
+    <SContainer>
+      <header>Todo App</header>
+      <div className="input-area">
+        <input type="text" placeholder="add todo" />
+        <button>Add</button>
+      </div>
+      <div className="filter-area">
+        <a href="#">All</a>
+        <a href="#">Todo</a>
+        <a href="#">Done</a>
+      </div>
+      <div className="todo-area">
+        <label>
+          <input type="checkbox" />
+          <span>aaa</span>
+        </label>
+        <label>
+          <input type="checkbox" />
+          <span>bbb</span>
+        </label>
+        <label>
+          <input type="checkbox" />
+          <span>ccc</span>
+        </label>
+      </div>
+      <p>3 todos</p>
+    </SContainer>
   );
 };
+
+const SContainer = styled.div`
+  width: 750px;
+  margin: 0 auto;
+  padding: 30px;
+`;
 
 export default App;
